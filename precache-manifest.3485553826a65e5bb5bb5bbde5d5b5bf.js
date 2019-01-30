00d419688ae30434823261dbb5833e660bbc5829
hari shankar bisen <bisenharishankar@gmail.com>
2019-01-30 10:52:28 +0530

    Updates

---
+++ precache-manifest.3485553826a65e5bb5bb5bbde5d5b5bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fac0f9bd19f6ff6f1c7",
+    "url": "/react-material-ui/static/css/main.650d09d2.chunk.css"
+  },
+  {
+    "revision": "7fac0f9bd19f6ff6f1c7",
+    "url": "/react-material-ui/static/js/main.7fac0f9b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-material-ui/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9b383eb29d37447c0359",
+    "url": "/react-material-ui/static/js/runtime~main.9b383eb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-material-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06bcb523e12fb0327291a66b69014a35",
+    "url": "/react-material-ui/index.html"
+  }
+];
